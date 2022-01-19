81bc4d3f9b3661fbcd724099adee94766d311a2a
RajMazing <caseyraj92@gmail.com>
2022-01-18 23:47:43 -0600

    refactored application.js, interviewlistitem, and interviewlist

--- src/components/InterviewList.js
+++ src/components/InterviewList.js
@@ -1,24 +1,32 @@
-import React from "react";
-import InterviewerListItem from "./InterviewerListItem";
 
+  
+import React from "react";
 import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
+import PropTypes from "prop-types";
 
 export default function InterviewerList(props) {
-  console.log(props);
-
-  const listOfInterviewers = props.interviewers.map(interviewer => {
-    if (interviewer.id === props.interviewer) {
-      return <InterviewerListItem key={interviewer.id} name={interviewer.name} id={interviewer.id} avatar={interviewer.avatar} setInterviewer={props.setInterviewer} selected />
-    } else {
-      return <InterviewerListItem key={interviewer.id} name={interviewer.name} id={interviewer.id} avatar={interviewer.avatar} setInterviewer={props.setInterviewer} />
-    }
+  const mappedList = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
+      />
+    );
   });
-  
+
   return (
     <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{listOfInterviewers}</ul>
+      <h4 className="interviewers__header text--light"></h4>
+      <ul className="interviewers__list">{mappedList}</ul>
     </section>
-
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+  setInterviewer: PropTypes.func.isRequired,
+};
--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,7 @@ import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
+import InterviewerList from "components/InterviewList";
 
 storiesOf("Button", module)
   .addParameters({
@@ -64,41 +65,68 @@ storiesOf("Button", module)
     ))
     .add("Tuesday", () => (
       <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
-    ))
-    .add("Wednesday", () => (
-        <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
     ));
-
-    const interviewer = {
-      id: 1,
-      name: "Sylvia Palmer",
-      avatar: "https://i.imgur.com/LpaY82x.png"
-    };
+  
+  const interviewer = {
+    id: 1,
+    name: "Sylvia Palmer",
+    avatar: "https://i.imgur.com/LpaY82x.png"
+  };
+  
+  storiesOf("InterviewerListItem", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+    })
+    .add("Unselected", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+      />
+    ))
+    .add("Selected", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected
+      />
+    ))
+    .add("Clickable", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={event => action("setInterviewer")(interviewer.id)}
+      />
+    ));
+  
+    const interviewers = [
+      { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+      { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+      { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+      { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+      { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+    ];
     
-    storiesOf("InterviewerListItem", module)
+    storiesOf("InterviewerList", module)
       .addParameters({
         backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
       })
-      .add("Unselected", () => (
-        <InterviewerListItem
-          id={interviewer.id}
-          name={interviewer.name}
-          avatar={interviewer.avatar}
+      .add("Initial", () => (
+        <InterviewerList
+          interviewers={interviewers}
         />
       ))
       .add("Selected", () => (
-        <InterviewerListItem
-          id={interviewer.id}
-          name={interviewer.name}
-          avatar={interviewer.avatar}
-          selected
+        <InterviewerList
+          interviewers={interviewers}
+          interviewer={3}
         />
       ))
       .add("Clickable", () => (
-        <InterviewerListItem
-          id={interviewer.id}
-          name={interviewer.name}
-          avatar={interviewer.avatar}
+        <InterviewerList
+          interviewers={interviewers}
           setInterviewer={action("setInterviewer")}
         />
       ));
